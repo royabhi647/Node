5924b9e83bf1a010f9f948ace82d0f6efdaf4271
royabhi647 <royabhi647@gmail.com>
2022-03-29 17:24:08 +0530

    file orgainzer

--- FileOrganizer/commands/help.js
+++ FileOrganizer/commands/help.js
@@ -1,21 +1,20 @@
-function help(){
+function help() {
     console.log(`
         These are some myCLI commands used in various situations:
-
-           1.node main.js tree <path>
-           2.node main.js organize <path>
-           3.node main.js help
+            1. node main.js tree <path>
+            2. node main.js organize <path>
+            3. node main.js help
     `);
 }
 
-// function abc(){
-//         console.log("in help.js")
-// }
 
+// function abc() {
+//     console.log("in help.js");
+// }
 
 module.exports = {
     //key value
-    help:help,
-    // haathi:help
-    //ghoda:abc
+    help: help,
+    // haathi: help
+    // ghoda:abc
 }
--- FileOrganizer/main.js
+++ FileOrganizer/main.js
@@ -1,32 +1,29 @@
-//Entry point of my command line
-let helpFunc=require("./commands/help");
+// entry point of my command line 
+let helpFunc = require("./commands/help");
+let orgFunc = require("./commands/organize");
+let treeFunc = require("./commands/tree");
 // console.log(helpFunc.ghoda());
-
 let inputArr = process.argv.slice(2);
 // console.log(inputArr);
-
 let command = inputArr[0];
 let path = inputArr[1];
-
-switch(command){    //organize
+switch (command) { //organizee
     case "tree":
         //call tree function
-        console.log("tree function called and executed successfully on path " + path);
+        treeFunc.tree(path);
+        // console.log("tree function called and executed succesfully on path " + path);
         break;
-
     case "organize":
         //call organize function
-        console.log("organize function called and executed successfully on path " + path);
+        orgFunc.organize(path);
+        // console.log("organize function called and executed succesfully on path "+ path);
         break;
-
     case "help":
         //call help function
         helpFunc.help();
-        //help function called and executed successfully
+        // console.log("help function called and executed succesfully");
         break;
-
     default:
-        console.log("command not recognized :/");
+        console.log("command not recognized :/")
         break;
-         
 }
