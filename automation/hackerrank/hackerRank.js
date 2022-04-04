fd956f14cf95e35142a53ca7c5db3f1cc16c389b
royabhi647 <royabhi647@gmail.com>
2022-04-04 08:53:23 +0530

    ::white_check_mark:Hackerrank algo click

--- automation/hackerrank/hackerRank.js
+++ automation/hackerrank/hackerRank.js
@@ -1,86 +1,74 @@
-const puppeteer = require("puppeteer");
-// let { email, password } = require('./secrets');
-let email = "royabhi647@gmail.com";
-let password = "Abhishek@123";
+const puppeteer=require("puppeteer");
+let email="royabhi647@gmail.com";
+let password="Abhishek@123";
 
 let curTab;
-let browserOPenPromise = puppeteer.launch({
-  headless: false,
-  defaultViewport: null,
-  args: ["--start-maximized"],
-  //chrome://version/
-  // executablePath:
-  //   "//Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-});
-browserOPenPromise //fulfill
-  .then(function (browser) {
-    console.log("browser is open");
-    // console.log(browser);
-    //An array of all open pages inside the Browser.
-    //returns an array with all the pages in all browser contexts
-    let allTabsPromise = browser.pages();
-    return allTabsPromise;
-  })
-  .then(function (allTabsArr) {
-    curTab = allTabsArr[0];
-    console.log("new tab");
-    //URL to navigate page to
-    let visitingLoginPagePromise = curTab.goto("https://www.hackerrank.com/auth/login");
-    return visitingLoginPagePromise;
-  })
-  .then(function (data) {
-    // console.log(data);
-    console.log("Hackerrank login page opened");
-    //selector(where to type), data(what to type)
-    let emailWillBeTypedPromise = curTab.type("input[name='username']", email);
-    return emailWillBeTypedPromise;
-  })
-  .then(function () {
-    console.log("email is typed");
-    let passwordWillBeTypedPromise = curTab.type("input[type='password']", password);
-    return passwordWillBeTypedPromise;
-  })
-  .then(function () {
-    console.log("password has been typed");
-    let willBeLoggedInPromise = curTab.click(
-      ".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled"
-    );
-    return willBeLoggedInPromise;
-  })
-  .then(function () {
-    console.log("logged into hackerrank successfully");
-    //waitAndClick will wait for the selector to load , and then click on the node
-    let algorithmTabWillBeOPenedPromise = waitAndClick(
-      "div[data-automation='algorithms']"
-    );
-    return algorithmTabWillBeOPenedPromise;
-  })
-  .then(function () {
-    console.log("algorithm pages is opened");
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
-function waitAndClick(algoBtn) {
-  let waitClickPromise = new Promise(function (resolve, reject) {
-    let waitForSelectorPromise = curTab.waitForSelector(algoBtn);
-    waitForSelectorPromise
-      .then(function () {
-        console.log("algo btn is found");
-        let clickPromise = curTab.click(algoBtn);
-        return clickPromise;
-      })
-      .then(function () {
-        console.log("algo btn is clicked");
-        // resolve();
-      })
-      .catch(function (err) {
-        console.log(err);
-      })
-  });
 
-  // waitClickPromise.then(function () {
-  //   console.log("inside then of waitclick");
-  // });
-  return waitClickPromise;
-}
+let browserOpenPromise=puppeteer.launch({
+  headless:false,
+  defaultViewport:null,
+  args:["--start-maximized"],
+});
+// console.log("browserOpenPromise");
+browserOpenPromise.then(function(browser){
+  console.log("browser is open");
+  console.log(browserOpenPromise);
+  // console.log(browser);
+
+  let allTabsPromise=browser.pages();
+  return allTabsPromise;
+})
+.then(function(allTabsArr){
+  curTab=allTabsArr[0];
+  console.log("new page");
+
+  let visitingLoginPagePromise=curTab.goto("https://www.hackerrank.com/auth/login");
+  return visitingLoginPagePromise;
+})
+.then(function(data){
+  // console.log(data);
+  console.log("Hackerrank login page opened");
+  let emailWillBeTypedPromise=curTab.type("input[name='username']",email);
+  return emailWillBeTypedPromise;
+})
+.then(function(){
+  console.log("email is typed");
+
+  let passwordWillBeTypedPromise=curTab.type("input[name='password']",password);
+  return passwordWillBeTypedPromise;
+})
+.then(function(){
+  console.log("password is typed");
+
+  let willBeLoggedInPromise=curTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+  return willBeLoggedInPromise;
+})
+.then(function(){
+  console.log("logged in into hackerrank successfully");
+
+  let algorithmTabWillBeOpenedPromise=waitAndClick("div[data-automation='algorithms']");     //
+  return algorithmTabWillBeOpenedPromise;                                                    //
+})                                                                                          //
+.then(function(){                                                                           //
+  console.log("algorithm page is opened");                                                //
+})                                                                                       //
+
+function waitAndClick(algoBtn){                                 //
+  let waitClickPromise=new Promise(function(resolve,reject){    // 
+    let waitForSelectorPromise=curTab.waitForSelector(algoBtn); //  
+    waitForSelectorPromise                                       //
+    .then (function(){                                           //
+      console.log("algo btn is found");                          //
+      let clickPromise=curTab.click(algoBtn);                    //
+      return clickPromise;                                        //
+    })                                                           //
+    .then (function(){                                          // 
+      console.log("algo btn is clicked");                       //
+      resolve();                                                 //
+    })                                                          //
+    .catch(function(err){                                       //
+      reject(err);                                             //
+    })                                                          //
+  });                                                           //
+  return waitClickPromise;                                    //
+}                                                             //
