cd766c9c6663a945edaeaed0edcda84533f63761
royabhi647 <royabhi647@gmail.com>
2022-04-13 09:26:18 +0530

    :fire:How async code working

--- automation/poc/setTimeOut.js
+++ automation/poc/setTimeOut.js
@@ -1,16 +1,40 @@
+// function yolo(){
+//     var a=10;
+//     function cb(err,res){
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             console.log("Hello How are you ? ");
+//         }
+//     }
+//      // function to be executed(callback fun) , time(ms) after which cb to be executed
+//      // 1 second =1000 milli second
+//     setTimeout(cb,5000);
+//     console.log(a);
+// }
+// yolo();
+// let b=100;
+// console.log(b);
+
+
+
+const request = require ("request");
+let url ="https://www.worldometers.info/coronavirus/"
 function yolo(){
     var a=10;
-    function cb(err,res){
+    function cb(err,res,body){
         if(err){
             console.log(err);
         }
         else{
-            console.log("Hello How are you ? ");
+            console.log("Hello how are you ? ");
+            console.log("statusCode is",res.statusCode);
         }
     }
-     // function to be executed(callback fun) , time(ms) after which cb to be executed
-     // 1 second =1000 milli second
-    setTimeout(cb,5000);
+    //function to be executed(callback fun) , time(ms) after which cb to be executed
+    // 1 second =1000 milli second
+    request(url,cb);
     console.log(a);
 }
 yolo();
