e5e4cbfe3c6ac0fd170498310b19b18060bf8beb
royabhi647 <royabhi647@gmail.com>
2022-04-13 09:39:37 +0530

    :fire:How async code is working

--- automation/poc/setTimeOut.js
+++ automation/poc/setTimeOut.js
@@ -15,28 +15,39 @@
 // }
 // yolo();
 // let b=100;
-// console.log(b);
+// console.log(b);                     //->output=10,100,Hello How are you?
 
 
 
-const request = require ("request");
-let url ="https://www.worldometers.info/coronavirus/"
-function yolo(){
-    var a=10;
-    function cb(err,res,body){
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log("Hello how are you ? ");
-            console.log("statusCode is",res.statusCode);
-        }
-    }
-    //function to be executed(callback fun) , time(ms) after which cb to be executed
-    // 1 second =1000 milli second
-    request(url,cb);
-    console.log(a);
+
+// const request = require ("request");
+// let url ="https://www.worldometers.info/coronavirus/"
+// function yolo(){
+//     var a=10;
+//     function cb(err,res,body){
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             console.log("Hello how are you ? ");
+//             console.log("statusCode is",res.statusCode);
+//         }
+//     }
+//     //function to be executed(callback fun) , time(ms) after which cb to be executed
+//     // 1 second =1000 milli second
+//     request(url,cb);
+//     console.log(a);
+// }
+// yolo();
+// let b=100;
+// console.log(b);                   //->output=10,100,Hello how are you?,statusCode is 200
+
+
+
+
+/* Print 1 to 10 on every 2 second */
+for(let i=1;i<=10;i++){
+    setTimeout(function(){
+        console.log(i);
+    },2000*i);
 }
-yolo();
-let b=100;
-console.log(b);
