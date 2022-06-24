9645275a209d0bd1eb3d3bc4590ba4a22df5f54f
royabhi647 <royabhi647@gmail.com>
2022-06-24 09:33:31 +0530

    :zap:automatic type conversion completed

---
+++ js/automaticTpyeConversion.js
@@ -0,0 +1,16 @@
+var ans="5" + 1;
+
+var ans=1 + "5";
+console.log(ans);
+
+console.log(null *5);   //0
+
+console.log(null * "5"); //0
+
+console.log(undefined * 5); //Nan
+
+console.log("5" - 1);     //4
+
+console.log("ten" * 3);    //Nan
+
+console.log("10" * 3);      //30
