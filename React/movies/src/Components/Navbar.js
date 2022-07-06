97ab9dcf43087b048a43dae7e1caa01e7554d0c9
royabhi647 <royabhi647@gmail.com>
2022-07-06 16:42:49 +0530

    :art:navbar and banner completed

---
+++ React/movies/src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class navbar extends Component {
+  render() {
+    return (
+      <div style = {{display:'flex' , backgroundColor:"lightblue" , padding:"1rem" , justifyContent:"center" , alignItems:"center"}}>
+        <h1>Movies app</h1>
+        <h2 style = {{marginLeft:"2rem"}}>Favourites</h2>
+      </div>
+    )
+  }
+}
