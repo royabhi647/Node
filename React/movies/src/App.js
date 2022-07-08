600e002a2f8967aa9f7681a73714b61e6f5d5ee8
royabhi647 <royabhi647@gmail.com>
2022-07-08 20:18:15 +0530

    :constructor:navbar & banner completed , list started

--- React/movies/src/App.js
+++ React/movies/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
+import List from './Components/List';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar/>
       <Banner/>
+      <List/>
     </>
   );
 }
--- React/movies/src/Components/Navbar.js
+++ React/movies/src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class navbar extends Component {
   render() {
     return (
-      <div style = {{display:'flex' , backgroundColor:"lightblue" , padding:"1rem" , justifyContent:"center" , alignItems:"center"}}>
+      <div style = {{display:'flex' , backgroundColor:"white" ,color:"blue", padding:"1rem" , justifyContent:"center" , alignItems:"center"}}>
         <h1>Movies app</h1>
         <h2 style = {{marginLeft:"2rem"}}>Favourites</h2>
       </div>
