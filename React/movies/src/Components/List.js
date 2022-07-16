6fae1a4ae549b1177ecce9a51f37137325f43e32
royabhi647 <royabhi647@gmail.com>
2022-07-16 19:44:43 +0530

    :bookmark:apply movieDB and life cycle

--- React/movies/src/Components/List.js
+++ React/movies/src/Components/List.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import { movies } from "./getMovies";
+// import { movies } from "./getMovies";
+import axios, { Axios } from "axios";
+import API_KEY from "../secrets";
 
 export default class List extends Component {
   constructor() {
     super();
+    console.log("constructor is called");
     this.state = {
       hover: "",
+      parr: [1],
+
+      currPage: 1,
+      movies: [],
     };
   }
 
@@ -21,13 +28,27 @@ export default class List extends Component {
     });
   };
 
+  async componentDidMount() {
+    console.log("componentDidMount is called");
+    let ans = await axios.get(
+      "https://api.themoviedb.org/3/movie/popular?api_key=c7ee8fb81b5b5a537ef10f0f68bcd0c2&language=en-US&page=2"
+      );
+    console.log(ans.data);
+
+    this.setState({
+      movies: [...ans.data.results]
+    });
+  }
+
   render() {
-    let movie = movies.results;
+    console.log("render is called");
+
+    // let movie = movies.results;
     return (
       <>
-        {movie.length == 0 ? (
-          <div class="spinner-border text-primary" role="status">
-            <span class="sr-only">Loading...</span>
+        {this.state.movies.length == 0 ? (
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
           </div>
         ) : (
           <div>
@@ -35,7 +56,7 @@ export default class List extends Component {
               <strong>Trending</strong>
             </h3>
             <div className="movies-list">
-              {movie.map((movieObj) => (
+              {this.state.movies.map((movieObj) => (
                 <div
                   className="card movie-card"
                   onMouseEnter={() => this.handleEnter(movieObj.id)}
@@ -54,7 +75,7 @@ export default class List extends Component {
                   {/* <p className="card-text movie-text">{movieObj.overview}</p> */}
                   <div className="button-wrapper">
                     {this.state.hover == movieObj.id && 
-                      <a href="#" class="btn btn-primary movie-button">
+                      <a href="#" class="btn btn-danger movie-button">
                         Add to Favourites
                       </a>
                     }
@@ -63,29 +84,25 @@ export default class List extends Component {
               ))}
               <div className="page-ination">
                 <nav aria-label="Page navigation example">
-                  <ul class="pagination">
-                    <li class="page-item">
-                      <a class="page-link" href="#">
+                  <ul className="pagination">
+                    <li className="page-item">
+                      <a className="page-link" href="#">
                         Previous
                       </a>
                     </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">
-                        1
-                      </a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">
-                        2
-                      </a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">
-                        3
-                      </a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">
+
+                    {
+                      this.state.parr.map((pageNum) => (
+                        <li className="page-item">
+                          <a className="page-link" href="#">
+                            {pageNum}
+                          </a>
+                        </li>
+                      ))
+                    }
+
+                    <li className="page-item">
+                      <a className="page-link" href="#">
                         Next
                       </a>
                     </li>
