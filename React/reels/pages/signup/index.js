a33e4a93f25a130627df7854d098e7d041b70fa3
royabhi647 <royabhi647@gmail.com>
2022-08-05 14:08:10 +0530

    :lipstick:Sign-up UI completed

--- React/reels/pages/signup/index.js
+++ React/reels/pages/signup/index.js
@@ -1,44 +1,78 @@
-import React from 'react'
-import TextField from '@mui/material/TextField';
-import Image from 'next/image'
-import logo from '../../assets/Instagram.jpeg'
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Image from "next/image";
+import logo from "../../assets/Instagram.jpeg";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 function index() {
   return (
     // <div>signup page</div>
-    <div className='signup-container'>
-        <div className='signup-card'>
-            <Image src={logo}/>
+    <div className="signup-container">
+      <div className="signup-card">
+        <Image src={logo} />
         <TextField
-             id="outlined-basic"
-             size='small'
-             label="Email"
-             variant="outlined"
-             fullWidth
-             margin='dense'
+          id="outlined-basic"
+          size="small"
+          label="Email"
+          variant="outlined"
+          fullWidth
+          margin="dense"
         />
 
         <TextField
-             id="outlined-basic"
-             size='small'
-             label="Password"
-             variant="outlined"
-             fullWidth
-             margin='dense'
-             type="password"
+          id="outlined-basic"
+          size="small"
+          label="Password"
+          variant="outlined"
+          fullWidth
+          margin="dense"
+          type="password"
         />
 
         <TextField
-             id="outlined-basic"
-             size='small'
-             label="Full Name"
-             variant="outlined"
-             fullWidth
-             margin='dense'
+          id="outlined-basic"
+          size="small"
+          label="Full Name"
+          variant="outlined"
+          fullWidth
+          margin="dense"
         />
+
+        <Button 
+          color="secondary" 
+          variant="contained" 
+          component="label"
+          fullWidth 
+          size="small" 
+        > 
+          <IconButton color="secondary">
+            <CloudUploadIcon />
+          </IconButton>
+          Upload Profile Image
+          <input hidden accept="image/*" multiple type="file" />
+        </Button>
+
+        <Button 
+          style={{marginTop:"1rem"}}
+          variant="contained" 
+          component="label"
+          fullWidth 
+         >
+          Sign Up
+        </Button>
+
+        <div className="tnc">
+          By signing up, you agree to our Terms, Conditions and Cookies policy.
         </div>
+      </div>
+      <div className="bottom-card">
+          Already Have an account ?{" "}
+          <span style={{ color: "blueviolet" }}>Login</span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
