c98006ca06bb78c5ee81fb054fe5aa52c52266c4
royabhi647 <royabhi647@gmail.com>
2022-09-18 13:32:14 +0530

    :tada:create timer

---
+++ React/timer/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './Components/Clock';
+
+function App() {
+  return (
+  // <h1>timer</h1>
+  <Clock />
+  );
+}
+
+export default App;
