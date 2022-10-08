493b3367c7bea1187b9ff6fd26f3beb350f5fb82
royabhi647 <royabhi647@gmail.com>
2022-10-08 16:24:33 +0530

    :tada:redux bat ball

---
+++ React/reduxdemo/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Bat from './Components/Bat';
+import { Provider } from 'react-redux';
+import store from './redux/store'
+import Ball from './Components/Ball';
+import React , { Component } from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <Provider store={store}>
+        <Bat />
+        <Ball />
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
