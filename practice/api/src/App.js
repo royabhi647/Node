f8353adce157ae3f5ac13e6187615a82213a9c2d
royabhi647 <royabhi647@gmail.com>
2022-10-11 10:50:53 +0530

    fetch API get method

--- practice/api/src/App.js
+++ practice/api/src/App.js
@@ -1,17 +1,42 @@
-import './App.css';
-import React,{useEffect} from 'react'
+import "./App.css";
+import React, { useEffect,useState } from "react";
 
+        //  fetch Api get method
 function App() {
-  fetch("./data.js").then((result)=>{
-    result.json().then((resp)=>{
-      console.log("result",resp);
-    })
-  })
+  const [data,setData]=useState([]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:5500/data/data.html").then((result) => {
+      result.json().then((resp) => {
+        // console.log("result", resp);
+        setData(resp);
+      });
+    });
+  },[]);
+  console.log(data);
+
   return (
     <div>
       <h1>Get API Call</h1>
+      <table border={1}>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Mobile</td>
+        </tr>
+        {
+          data.map((item)=>
+            <tr>
+            <td>{item.userId}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.mobile}</td>
+          </tr>
+          )
+        }
+      </table>
     </div>
-  )
+  );
 }
 
 export default App;
