09694708b7e67f52eacfd833677f9cb06e80977b
royabhi647 <royabhi647@gmail.com>
2022-10-17 11:07:54 +0530

    :memo:pass function as props

---
+++ practice/props2/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './User';
+import Members from './Members';
+
+function App() {
+  function getData()
+  {
+    alert('hello from app')
+  }
+  return (
+    <div>
+      <User data={getData}/>
+      <User data={getData}/>
+      <User data={getData}/>
+      <User data={getData}/>
+      <div style={{float:'right'}}>
+        <Members data={getData}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
