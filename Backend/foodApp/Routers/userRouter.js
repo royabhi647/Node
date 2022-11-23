c256bebeabb72426123fb08c0c9118720ccb775b
royabhi647 <royabhi647@gmail.com>
2022-11-23 21:43:15 +0530

    :fire: mvc architecture

--- Backend/foodApp/Routers/userRouter.js
+++ Backend/foodApp/Routers/userRouter.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const userRouter = express.Router();
-const userModel = require("../models/userModel");
-const jwt = require('jsonwebtoken');
-const JWT_KEY = 'khdlkhdsakfjlajvgldsj';
+const {getUsers, postUser, updateUser, deleteUser, getUserById, setCookies, getCookies} = require('../controller/userController');
+const {protectRoute} = require('../helper');
 
 userRouter
   .route("/")
@@ -14,85 +13,5 @@ userRouter
 userRouter.route("/setcookies").get(setCookies);
 userRouter.route("/getcookies").get(getCookies);
 userRouter.route("/:name").get(getUserById);
-
-// let isLoggedIn = true;
-//isadmin cookie can be used to identify b/w user and admin
-function protectRoute(req, res, next) {
-  if(req.cookies.login){
-    let token = req.cookies.login;
-    let isVerified = jwt.verify(token,JWT_KEY);
-    if(isVerified)  next();
-    else{
-      req.json({
-        msg:'user not verified'
-      })
-    }
-  }else{
-    return res.json({
-      msg : 'operation not allowed'
-    })
-  }
-} 
-
-async function getUsers(req, res) {
-  console.log(req.query);
-  let { name, age } = req.query;
-
-  // get all users from db
-  let allUsers = await userModel.find();
-
-  res.json({ msg: "users retrieved", allUsers });
-}
-
-function postUser(req, res) {
-  console.log(req.body.name);
-  //then i can put this in db
-  user.push(req.body);
-  res.json({
-    message: "Data received successfully",
-    user: req.body,
-  });
-}
-
-async function updateUser(req, res) {
-  console.log(req.body);
-  let dataToBeupdated = req.body;
-  // for(key in dataToBeupdated){
-  //     user[key] = dataToBeupdated[key];
-  // }
-  let doc = await userModel.findOneAndUpdate(
-    { email: "kumar@gmail.com" },
-    dataToBeupdated
-  );
-  res.json({
-    message: "Data updated successfully",
-  });
-}
-
-async function deleteUser(req, res) {
-  // user = {};
-  let doc = await userModel.deleteOne({ email: "krishna@gmail.com" });
-  console.log(doc);
-  res.json({
-    msg: "User has been deleted",
-  });
-}
-
-function getUserById(req, res) {
-  console.log(req.params.name);
-  res.json({ msg: "user id is ", obj: req.params });
-}
-
-function setCookies(req, res) {
-  res.cookie("isLoggedIn", false, { maxAge: 10000, secure: true });
-  res.cookie("password", 12345678, { secure: true });
-  res.send("cookie has been set");
-}
-
-function getCookies(req, res) {
-  let cookies = req.cookies.password;
-  console.log(cookies);
-  res.send("cookies received");
-}
 
 module.exports = userRouter;
---
+++ Backend/foodApp/controller/userController.js
@@ -0,0 +1,63 @@
+const userModel = require('../models/userModel');
+
+
+module.exports.getUsers=async function (req, res) {
+    console.log(req.query);
+    let { name, age } = req.query;
+  
+    // get all users from db
+    let allUsers = await userModel.find();
+  
+    res.json({ msg: "users retrieved", allUsers });
+  }
+  
+  module.exports.postUser=function (req, res) {
+    console.log(req.body.name);
+    //then i can put this in db
+    user.push(req.body);
+    res.json({
+      message: "Data received successfully",
+      user: req.body,
+    });
+  }
+  
+  module.exports.updateUser=async function (req, res) {
+    console.log(req.body);
+    let dataToBeupdated = req.body;
+    // for(key in dataToBeupdated){
+    //     user[key] = dataToBeupdated[key];
+    // }
+    let doc = await userModel.findOneAndUpdate(
+      { email: "kumar@gmail.com" },
+      dataToBeupdated
+    );
+    res.json({
+      message: "Data updated successfully",
+    });
+  }
+  
+  module.exports.deleteUser=async function (req, res) {
+    // user = {};
+    let doc = await userModel.deleteOne({ email: "krishna@gmail.com" });
+    console.log(doc);
+    res.json({
+      msg: "User has been deleted",
+    });
+  }
+  
+  module.exports.getUserById=function (req, res) {
+    console.log(req.params.name);
+    res.json({ msg: "user id is ", obj: req.params });
+  }
+  
+  module.exports.setCookies=function (req, res) {
+    res.cookie("isLoggedIn", false, { maxAge: 10000, secure: true });
+    res.cookie("password", 12345678, { secure: true });
+    res.send("cookie has been set");
+  }
+  
+  module.exports.getCookies=function (req, res) {
+    let cookies = req.cookies.password;
+    console.log(cookies);
+    res.send("cookies received");
+  }
