368332c509f6f581a8a91a6dea9814bc161b8ae2
royabhi647 <royabhi647@gmail.com>
2022-11-29 10:48:10 +0530

    :sparkles: array-string level 2 questions

---
+++ Data-Structure/level2/Arrays and Strings/pattern.js
@@ -0,0 +1,77 @@
+1. Range Addition
+2. Container With Most Water
+3. Squares Of A Sorted Array
+4. Majority Element - I
+5. Majority Elements - I I
+6. Majority Element General
+7. Next Greater Element I I I
+8. Product Of Array Except Itself Without Using Division Operator
+9. Partition Labels
+10. Partition Array Into Disjoint Intervals
+11. Max Chunks To Make Array Sorted
+12. Max Chunks To Make Array Sorted 2
+13. Min Jumps With +i -i Moves
+14. Max Product Of Three Numbers
+15. Sort Array By Parity
+16. Best Meeting Point
+17. Sieve Of Eratosthenes
+18. Segmented Sieve
+19. Transpose Of Matrix With Dimension M X N
+20. Transpose Of Matrix With Dimension N X N
+21. Shortest Unsorted Continuous Subarray
+22. Rotate Image
+23. Reverse Vowels Of A String
+24. Wiggle Sort 1
+25. Wiggle Sort 2
+26. Add Strings
+27. Multiply Strings
+28. Complex Number Multiplication
+29. Valid Palindrome 2
+30. Car Fleet
+31. Max Area Of Island
+32. Number Of Subarrays With Bounded Maximum
+33. Maximum Swap
+34. Smallest Range Covering Elements From K Lists
+35. Minimum Domino Rotations For Equal Row
+36. 2 Sum - Target Sum Unique Pairs
+37. Boats To Save People
+38. 3 Sum - Target Sum Unique Triplet
+39. 4 Sum - Target Sum With Unique Quadruple
+40. K Sum - Target Sum Unique Set
+41. First Missing Positive
+42. Pascal's Triangle 2
+43. Find All Duplicates In An Array
+44. Find And Replace Pattern
+45. Consecutive Numbers Sum
+46. Push Dominoes
+47. Maximum Product Subarray
+48. Sum Of Subsequence Widths
+49. The K-th Lexicographical String Of All Happy Strings Of Length N
+50. Construct Target Array With Multiple Sums
+51. Furthest Building You Can Reach
+52. Where Will The Ball Fall
+53. Ways To Make A Fair Array
+54. Orderly Queue
+55. Sort The Matrix Diagonally
+56. Reorganize String
+57. Bulb Switcher Iv
+58. Design Tic-tac-toe
+59. Maximum Sum Of Two Non-overlapping Subarrays
+60. Island Perimeter
+61. Largest Multiple Of Three
+62. Battleship In Board
+63. Merge Intervals
+64. Minimum Number Of Platform
+65. Meeting Rooms I
+66. Meeting Rooms I I
+67. Interval List Intersections
+68. Insert Intervals
+69. Find Minimum Number Of Arrows Needed To Burst Balloon
+70. Car Pooling
+71. Maximum Average Subarray I
+72. Min Len Of String After Removing Similar Ends
+73. Justification Of Text
+74. Set Matrix Zeroes
+75. Number Of Sub-arrays With Odd Sum
+76. Top K Frequent Elements
+77. Word Search
