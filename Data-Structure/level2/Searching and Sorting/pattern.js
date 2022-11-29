6db20746d6ebd636f59d790ffa1d50cd98e9e002
royabhi647 <royabhi647@gmail.com>
2022-11-29 15:29:18 +0530

    :sparkles: searching and sorting questions pattern

---
+++ Data-Structure/level2/Searching and Sorting/pattern.js
@@ -0,0 +1,45 @@
+1. Marks Of Pcm
+2. Union Of Two Sorted Arrays
+3. Search A 2d Matrix
+4. Search A 2d Matrix - 2
+5. Find Pivot Index
+6. Find K Closest Elements
+7. Find Pair With Given Difference
+8. Roof Top
+9. Maximize Sum Of Arr[i]*i Of An Array
+10. Count Inversions
+11. Find First And Last Postion Of Element In Sorted Array
+12. Max Sum In The Configuration
+13. Search In Rotated Sorted Array
+14. Find Minimum In Rotated Sorted Array
+15. Find Rotation Count
+16. Median Of Two Sorted Arrays
+17. Koko Eating Bananas
+18. Find The Smallest Divisor Given A Threshold
+19. Chocolate Distribution
+20. Allocate Minimum Number Of Pages
+21. Split Array Largest Sum
+22. Capacity To Ship Packages Within D Days
+23. Count The Triplets
+24. Count Possible Triangles
+25. Count Zeros In A Sorted Matrix
+26. Counting Elements In Two Arrays
+27. Count Zeros Xor Pairs
+28. Facing The Sun
+29. Distinct Absolute Array Elements
+30. Find Transition Point
+31. First Bad Version
+32. Guess Number Higher Or Lower
+33. Find The Element That Appears Once In Sorted Array
+34. Maximum Number Of 1's Row
+35. Heaters
+36. Largest Number
+37. Largest Perimeter Triangle
+38. Punish The Students
+39. Leaders In An Array
+40. Ishaan And Sticks
+41. Toppers Of Class
+42. Matrix Cells In Distance Order
+43. Sequential Digits
+44. K-th Smallest Prime Fraction
+45. Pancake Sorting
