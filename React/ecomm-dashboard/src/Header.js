2657da26f57add26be22949caa677dd543479f76
royabhi647 <royabhi647@gmail.com>
2022-12-02 21:04:56 +0530

    route setup

---
+++ React/ecomm-dashboard/src/Header.js
@@ -0,0 +1,21 @@
+import {Navbar, Nav} from 'react-bootstrap'
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+        {/* navbar */}
+      <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto nav_bar_wrapper">
+            <Link to="/add">Add Product</Link>
+            <Link to="/update">Update Product</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
