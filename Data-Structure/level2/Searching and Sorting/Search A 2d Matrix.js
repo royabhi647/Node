64de237fca9a9780957451b064c3c8e09ec3cd67
royabhi647 <royabhi647@gmail.com>
2022-12-30 15:41:54 +0530

    Search A 2d Matrix

--- Data-Structure/level2/Searching and Sorting/Search A 2d Matrix.js
+++ Data-Structure/level2/Searching and Sorting/Search A 2d Matrix.js
@@ -24,7 +24,7 @@ Sample Output
     true
 
 
-// code
+// 1st code
 
 import java.util.*;
 import java.io.*;
@@ -52,6 +52,87 @@ public class Main {
             }
         }
         return false;
+    }
+
+    public static void main(String[]args) {
+        //input work
+        Scanner scn = new Scanner(System.in);
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+
+        int[][]matrix = new int[m][n];
+
+        for(int i=0; i < m;i++) {
+            for(int j=0; j < n;j++) {
+                matrix[i][j] = scn.nextInt();
+            }
+        }
+
+        int target = scn.nextInt();
+
+        boolean isFound = search(matrix,target);
+        System.out.println(isFound);
+
+    }
+}
+
+
+// 2nd code
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    /*your task is to complete this function which returns true if target exists in the matrix
+    else return false*/
+    public static boolean search(int[][]matrix,int target) {
+        //write your code here
+        int r = binarySearchRowSelect(matrix,target);
+        
+        if(r == -1){
+            return false;
+        }
+        
+        boolean isFound = binarySearch(matrix, r, target);
+        return isFound;
+    }
+    
+    public static boolean binarySearch(int[][] matrix, int r, int target){
+        int low = 0;
+        int high = matrix[0].length - 1;
+        
+        while(low <= high){
+            int mid = (low + high) / 2;
+            
+            if(matrix[r][mid] == target){
+                return true;
+            }else if(matrix[r][mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+    
+    public static int binarySearchRowSelect(int[][]matrix, int target){
+        int low = 0;
+        int high = matrix.length - 1;
+        int lc = matrix[0].length - 1;  // last column
+        
+        while(low <= high){
+            int mid = (low + high) / 2;
+            
+            if(matrix[mid][0] <= target && target <= matrix[mid][lc]){
+                return mid;
+            }else if(matrix[mid][0] < target){
+                low = mid + 1;
+            }else if(matrix[mid][0] > target){
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[]args) {
