d446e5af4a32c8fa13fe09233d3c1f817e196377
royabhi647 <royabhi647@gmail.com>
2023-01-03 16:47:06 +0530

    graph questions pattern

---
+++ Data-Structure/level2/Graphs/pattern.js
@@ -0,0 +1,49 @@
+1. Coloring A Border
+    https://leetcode.com/problems/coloring-a-border/
+2. Number Of Enclaves
+3. Number Of Distinct Island
+4. Zero One Matrix
+5. Rotting Oranges
+6. As Far From Land As Possible
+7. Shortest Bridge
+8. Bus Routes
+9. Sliding Puzzle
+10. Minimum Number Of Swaps Required To Sort An Array
+11. Pepcoder And Reversing
+12. Pepcoding Course Schedule
+13. Alien Dictionary
+14. Kruskal Algorithm
+15. Optimize Water Distribution
+16. Swim In Rising Water
+    https://leetcode.com/problems/swim-in-rising-water/
+17. Minimum Cost To Connect All Cities
+18. Bellman Ford
+19. Negative Weight Cycle Detection
+20. Kosaraju Algorithm
+21. Mother Vertex
+22. Articulation Point
+23. Critical Connection
+24. Remove Max Number Of Edges To Keep Graph Fully Traversable
+25. Number Of Island 2
+26. Regions Cut By Slashes
+27. Reconstruct Itinerary
+    https://leetcode.com/problems/reconstruct-itinerary/
+28. Rank Transform Of A Matrix
+    https://leetcode.com/problems/rank-transform-of-a-matrix/
+29. Accounts Merge
+    https://leetcode.com/problems/accounts-merge/
+30. Minimize Malware Spread
+    https://leetcode.com/problems/minimize-malware-spread/
+31. Minimize Malware Spread 2
+    https://leetcode.com/problems/minimize-malware-spread-ii/
+32. Redundant Connection
+33. Minimize Hamming Distance After Swap Operations
+    https://leetcode.com/problems/minimize-hamming-distance-after-swap-operations/
+34. Redundant Connection 2
+35. Satisfiability Of Equality Equation
+36. Sentence Similarity
+37. Park Regions
+38. Kill The Most Monsters
+39. Number Of Connections To Make Pipeline Connected
+40. Number Of Provinces
+    https://leetcode.com/problems/number-of-provinces/
