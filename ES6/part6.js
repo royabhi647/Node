253b1226fe8699fba1047222ab1aaf3a709a3630
royabhi647 <royabhi647@gmail.com>
2023-01-10 15:41:28 +0530

    Not defined and undefined

---
+++ ES6/part6.js
@@ -0,0 +1,11 @@
+// Not defined and undefined in javascript
+
+// both are belongs to memory space, but in case of not defined they are not present in memory
+// in case of undefined, starting time memory is allocated of every variable is undefined after that 
+// when our code is executed memory allocation of variable is replaced with value.
+
+console.log(a);    //undefined
+var a = "Ajay"
+console.log(a);    // Ajay
+
+console.log(b);   //b is not defined
