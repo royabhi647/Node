048bffcdcda0f8a83688359cc96c4d4d0998a8f0
royabhi647 <royabhi647@gmail.com>
2023-01-31 20:34:12 +0530

    Throttling

--- ES6/part15.js
+++ ES6/part15.js
@@ -1,2 +1,19 @@
 // Throttling in JavaScript
+// Throttling is a technique in which, no matter how many times the user fires the event,
+// the attached function will be executed only once in a given time interval.
 
+
+const mythrottle=(fn,d)=>{
+    return function(...args){
+        document.getElementById("myid").disabled=true;
+        setTimeout(()=>{
+            fn();
+        },d)
+    }
+}
+
+
+const newFun = mythrottle(()=>{
+    document.getElementById("myid").disabled=false;
+    console.log("User clicked")
+},1000)
