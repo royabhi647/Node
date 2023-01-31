25b9a16aa5b090b1764dfeb07c705ca8457d78de
royabhi647 <royabhi647@gmail.com>
2023-01-31 19:49:17 +0530

    Debounce

---
+++ ES6/part14.js
@@ -0,0 +1,29 @@
+// Debounce
+ // -> In JavaScript, a debounce function makes sure that your code is only triggered once per user input.
+ // Search box suggestions, text-field auto-saves,
+ // and eliminating double-button clicks are all use cases for debounce.
+
+
+ // In JavaScript, the use case is similar. We want to trigger a function, but only once per use case.
+ // Let's say that we want to show suggestions for a search query,
+ // but only after a visitor has finished typing it.
+
+
+let counter = 0;
+
+function getData(){
+    console.log("fetching Data"+counter++);
+}
+
+
+function myDebounce(call,d){
+    let timer;
+    return function(...args){
+        if(timer) clearTimeout(timer);
+        setTimeout(()=>{
+            call();
+        },d)
+    }
+}
+
+const BetterFunction = myDebounce(getData,1000);
