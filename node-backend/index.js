f672aea4a953ceabc3176c7db24a1b5b32bdf139
royabhi647 <royabhi647@gmail.com>
2023-02-02 00:23:55 +0530

    backend started

---
+++ node-backend/index.js
@@ -0,0 +1,15 @@
+var a = 20;
+var b = 30;
+var c = 40;
+
+console.log(a+b+c);
+
+
+// filter
+const arr = [2,4,7,1,3,8,3];
+
+let result = arr.filter((item)=>{
+    return item===3
+})
+
+console.log(result)
