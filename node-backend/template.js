87f4887c5bd81e3acd32c2b1511f2dedd0387927
royabhi647 <royabhi647@gmail.com>
2023-02-15 20:05:02 +0530

    ejs template

---
+++ node-backend/template.js
@@ -0,0 +1,30 @@
+// remove extension from URL
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const publicPath = path.join(__dirname,'public')
+
+app.set('view engine','ejs');
+
+app.get('',(_,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+// template -> build dynamic page in node using ejs template package, ejs supports views folder
+app.get('/profile',(_,res)=>{
+    let user = {
+        name:'Abhishek Kumar',
+        email:'abhishek@test.com',
+        city:"Arrah"
+    }
+    res.render('profile',{user});
+})
+
+app.get('/about',(_,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+
+
+app.listen(5000);
