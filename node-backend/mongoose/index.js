d3a2244d27f4d42d82794380418282430b351ad3
royabhi647 <royabhi647@gmail.com>
2023-03-01 14:50:52 +0530

    CRUD with mongoose

--- node-backend/mongoose/index.js
+++ node-backend/mongoose/index.js
@@ -1,15 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const main = async() => {
-    await mongoose.connect("mongodb://localhost:27017/e-comm");
-    const ProductSchema = new mongoose.Schema({
-        name:String,
-        price:Number
-    });
-    const ProductsModel = mongoose.model('products',ProductSchema);
-    let data = new ProductsModel({name:"m8",price:1000})
-    let result = await data.save();
-    console.log(result);
+mongoose.connect("mongodb://localhost:27017/e-comm");
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  brand: String,
+  category: String,
+});
+
+// save data in mongodb
+const saveInDB = async () => {
+  const ProductsModel = mongoose.model("products", ProductSchema);
+  let data = new ProductsModel({
+    name: "m10",
+    price: 1000,
+    brand: "max 10",
+    category: "Mobile",
+  });
+  let result = await data.save();
+  console.log(result);
+};
+
+// saveInDB();
+
+
+// update data in mongodb
+const updateInDB = async () => {
+    const Product = mongoose.model('products',ProductSchema);
+    let data = await Product.updateOne(
+        {name:"m0"},
+        {$set: {price:750,name:"m13"}}
+    )
+    console.log(data);
 }
 
-main();
+// updateInDB();
+
+
+// Delete data from mongodb
+
+const deleteInDB =async () => {
+    const Product = mongoose.model('products',ProductSchema);
+    let data = await Product.deleteOne({name:"m8"});
+    console.log(data);
+}
+
+// deleteInDB();
+
+// read all data from mongodb
+const findInDB =async () => {
+    const Product = mongoose.model('products',ProductSchema);
+    let data = await Product.find();   // read all data
+    // let data = await Product.find({name:"m8"})  // search
+    console.log(data);
+}
+
+findInDB();
