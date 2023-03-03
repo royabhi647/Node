619bae4ef32f0f061bf59ea147de92326812ad7e
royabhi647 <royabhi647@gmail.com>
2023-03-03 11:11:12 +0530

    post, get, delete, update with mongoose

--- node-backend/mongoose1/index.js
+++ node-backend/mongoose1/index.js
@@ -1,16 +1,45 @@
-const express = require('express');
-require('./config') // db connection
-const Product = require('./product')  // model,schema
+const express = require("express");
+require("./config"); // db connection
+const Product = require("./product"); // model,schema
 
-const app = express();    // for make app executable
-app.use(express.json());  // convert postman body data into json otherwise we will get undefined
+const app = express(); // for make app executable
+app.use(express.json()); // convert postman body data into json otherwise we will get undefined
+
 
 // post api with mongoose
-app.post("/create",async(req,resp)=>{
-    let data = new Product(req.body);
-    let result = await data.save();
-    console.log(result);
-    resp.send(result);
+app.post("/create", async (req, resp) => {
+  let data = new Product(req.body);
+  let result = await data.save();
+  console.log(result);
+  resp.send(result);
+});
+
+
+// get api with mongoose
+app.get("/list", async (req, resp) => {
+  let data = await Product.find();
+  resp.send(data);
+});
+
+
+//delete api with mongoose
+app.delete("/delete/:_id", async (req, resp) => {
+  console.log(req.params);
+  let data = await Product.deleteOne(req.params);
+  resp.send(data);
+});
+
+
+// put api | update data
+app.put("/update/:_id", async (req, resp) => {
+  console.log(req.params);
+  let data = await Product.updateOne(
+        req.params,
+        { 
+            $set: req.body 
+        }
+    );
+  resp.send(data);
 });
 
 app.listen(5000);
