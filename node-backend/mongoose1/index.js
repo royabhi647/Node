00ab0a43c1839f36b3107585e26799b264be6a48
royabhi647 <royabhi647@gmail.com>
2023-03-03 11:43:41 +0530

    post, get, put, delete, search with mongoose

--- node-backend/mongoose1/index.js
+++ node-backend/mongoose1/index.js
@@ -42,4 +42,20 @@ app.put("/update/:_id", async (req, resp) => {
   resp.send(data);
 });
 
+
+// search data
+app.get("/search/:key",async(req,resp)=>{
+    console.log(req.params.key);
+    let data = await Product.find(
+        {
+            $or:[
+                {"name":{$regex:req.params.key}},
+                {"brand":{$regex:req.params.key}}
+            ]
+        }
+    )
+    resp.send(data);
+})
+
+
 app.listen(5000);
