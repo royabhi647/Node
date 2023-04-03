b205bc199a57c28149ba81da41eb52be75fad389
royabhi647 <royabhi647@gmail.com>
2023-04-03 08:52:53 +0530

    DP level-2 pattern

---
+++ Data-Structure/level2/DP/pattern.js
@@ -0,0 +1,86 @@
+1. Largest Square Sub-matrix With All 1's
+2. Print All Paths With Minimum Jumps
+3. Print All Paths With Minimum Cost
+4. Print All Paths With Maximum Gold
+5. Print All Paths With Target Sum Subset
+6. Print All Results In 0-1 Knapsack
+7. 2 Key Keyboard
+8. 4 Key Keyboard
+9. Longest Increasing Subsequence
+10. Print All Longest Increasing Subsequences
+11. Maximum Sum Increasing Subsequence
+12. Longest Bitonic Subsequence
+13. Maximum Non-overlapping Bridges
+14. Russian Doll Envelopes
+15. Min Squares
+16. Catalan Number
+17. Number Of Bsts
+18. Count Of Valleys And Mountains
+19. Count Brackets
+20. Circle And Chords
+21. Number Of Ways Of Triangulation
+22. Catalan Numbers Variation
+23. Matrix Chain Multiplication
+24. Burst Balloons
+25. Minimum Score Of Triangulation
+26. Boolean Parenthesization
+27. Optimal Binary Search Tree
+28. Rod Cutting
+29. Minimum Palindromic Cut
+30. Longest Common Subsequence
+31. Longest Palindromic Subsequences
+32. Count Palindromic Subsequences
+33. Count Palindromic Substrings
+34. Longest Palindromic Substring
+35. Count Distinct Subsequences
+36. Count Of Distinct Palindromic Subsequences
+37. Wildcard Pattern Matching
+38. Regular Expression Matching
+39. Longest Common Substring
+40. Longest Repeating Subsequence
+41. Edit Distance
+42. Scramble String
+43. Minimum Ascii Delete Sum For Two Strings
+44. Minimum Cost To Make Two Strings Identical
+45. Kadane's Algorithm
+46. K Concatenation
+47. Maximum Sum Subarray With At Least K Elements
+48. Egg Drop
+49. Optimal Strategy For A Game
+50. Cherry Pickup
+51. Probability Of Knight In The Chessboard
+52. Highway Billboard
+53. Distinct Transformations
+54. Numeric Keypad
+55. Maximum Difference Of Zeros And Ones In Binary String
+56. Maximum Sum Of Two Non-overlapping Subarrays
+57. Maximum Sum Of Three Non-overlapping Subarrays
+58. Maximum Sum Of M Non-overlapping Subarrays
+59. Arithmetic Slices 1
+60. Arithmetic Slices 2
+61. Word Break
+62. Temple Offerings
+63. Minimum Insertions To Make Palindrome
+64. Ugly Number
+65. Super Ugly Number
+66. Find Water In Glass
+67. Frog Jump
+68. Interleaving Of Two Strings
+69. Maximum Length Of Repeated Subarray
+70. Distinct Echo Substrings
+    --> https://leetcode.com/problems/distinct-echo-substrings/
+71. Minimum Cost To Cut A Stick
+    --> https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
+72. Delete And Earn
+    --> https://leetcode.com/problems/delete-and-earn/
+73. Number Of Good Ways To Split A String
+    --> https://leetcode.com/problems/number-of-good-ways-to-split-a-string/
+74. Minimum Cost For Tickets
+75. Minimum Ascii Delete Sum For Two Strings
+    --> https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/
+76. Maximum Alternating Subsequence Sum
+    --> https://leetcode.com/problems/maximum-alternating-subsequence-sum/
+77. Stone Game
+    --> https://leetcode.com/problems/stone-game-vii/
+78. Largest Multiple Of Three
+    --> https://leetcode.com/problems/largest-multiple-of-three/
